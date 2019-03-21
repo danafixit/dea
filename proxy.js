7f3133f8862ea0a15df399f288f50e5d94b40a22
danafixit <48789680+danafixit@users.noreply.github.com>
2019-03-21 16:32:32 +0700

    Add files via upload

---
+++ proxy.js
@@ -0,0 +1,38 @@
+function FindProxyForURL(url, host)
+{
+	//if (true) return "DIRECT";
+
+    // Proxy server in format "PROXY [proxy server]:[proxy port]"
+    var proxy = "PROXY proxy.pc.danafix.net:31280";
+     
+    // Proxy Exceptions:
+    var exceptions = new Array(     
+        /\.local$/,
+        /\.dsi$/,
+        /\.fineasy\.id$/,
+        /^fineasy\.id$/,
+        /\.skype\.com$/,
+        /\.mfi-ap\.asia$/
+    );
+     
+    for (i = 0; i < exceptions.length; i++) // Iterate through each exception 
+    {
+        if (exceptions[i].test(host)) // Test regex query against hostname
+        {
+            return "DIRECT"; // Bypass the proxy
+        }
+    }
+    
+    var hostIP = dnsResolve(host);
+    
+    if (isInNet(hostIP, '10.0.0.0', '255.0.0.0') ||
+	    isInNet(hostIP, '127.0.0.0', '255.0.0.0') ||
+	    isInNet(hostIP, '169.254.0.0', '255.255.0.0') ||
+	    isInNet(hostIP, '172.16.0.0', '255.240.0.0') ||
+	    isInNet(hostIP, '192.168.0.0', '255.255.0.0'))
+    {
+	    return 'DIRECT';
+    }
+     
+    return proxy; // Connect via proxy
+}
